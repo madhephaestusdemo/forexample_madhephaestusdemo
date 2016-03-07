1eee41bfe01a5948d7b748d033059077491c7f2f
admin <admin@192.168.0.14>
2016-03-07 17:24:58 -0500

    changing the loop demo/n/n

--- forexample.groovy
+++ forexample.groovy
@@ -1,5 +1,20 @@
-println "Print all numbers from 0 to 999"
-for (int i = 0; i < 1000; i += 1)
+int size=40
+CSG cube = new Cube(	size,// X dimention
+			size,// Y dimention
+			size//  Z dimention
+			).toCSG()
+for (int i = 0; i < 10; i += 1)
 {
- println i
+	cube=cube.union(
+		new Cube(	size,// X dimention
+			size,// Y dimention
+			size//  Z dimention
+			).toCSG()
+			.rotz(i*10)
+			.movex(i*5.5)
+			.movez(i*5)
+			
+	)
 }
+
+return cube
