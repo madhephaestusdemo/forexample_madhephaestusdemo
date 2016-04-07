3f98c4ac265193587eb5e64845b5ececcfe64606
madhephaestus <mad.hephaestus@gmail.com>
2016-04-07 16:10:48 -0400

    fixing sample for loop/n/n

--- forexample.groovy
+++ forexample.groovy
@@ -1,20 +1,4 @@
-int size=40
-CSG cube = new Cube(	size,// X dimention
-			size,// Y dimention
-			size//  Z dimention
-			).toCSG()
 for (int i = 0; i < 10; i += 1)
 {
-	cube=cube.union(
-		new Cube(	size,// X dimention
-			size,// Y dimention
-			size//  Z dimention
-			).toCSG()
-			.rotz(i*10)
-			.movex(i*5.5)
-			.movez(i*5)
-			
-	)
+	println "The loop count is: "+i 
 }
-
-return cube
